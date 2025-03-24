b697b145f06a95101a1f74f899d752d78893bcf5
tanmayee05 <2200031253@kluniversity.in>
2025-03-24 21:49:34 +0530

    commit

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -40,7 +40,7 @@ const MyOrders = () => {
                 <p>{currency}{order.amount}.00</p>
                 <p>Items: {order.items.length}</p>
                 <p><span>&#x25cf;</span> <b>{order.status}</b></p>
-                <button >Cancel Order</button>
+                <button onClick={fetchOrders}>Track Order</button>
             </div>
           )
         })}
