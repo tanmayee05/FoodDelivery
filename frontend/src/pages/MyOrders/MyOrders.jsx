66cba9490dde54d710f92f053575abc0c4abca26
tanmayee05 <2200031253@kluniversity.in>
2025-03-26 18:57:38 +0530

    stripe key

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -40,7 +40,6 @@ const MyOrders = () => {
                 <p>{currency}{order.amount}.00</p>
                 <p>Items: {order.items.length}</p>
                 <p><span>&#x25cf;</span> <b>{order.status}</b></p>
-                <button onClick={fetchOrders}>Track Order</button>
             </div>
           )
         })}
